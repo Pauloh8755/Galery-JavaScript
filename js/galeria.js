31cd157048a190388551296defd68485a40a57aa
PAULO HENRIQUE DE ANDRADE JUNIOR <21193420@CFP127ALUNO.LOCAL>
2021-10-19 13:11:23 -0300

    Galeria concluida

--- js/galeria.js
+++ js/galeria.js
@@ -22,7 +22,6 @@ const filtrarId= item =>{
 
 }
 //função para criar itens da caleria dentro do container-galeria a partir da url
-//utilizando += para concatenar innerHTML anterior com o proximo 
 const criarItem = item =>{
     const container = document.querySelector(".container-galeria");
     //Criando um novo elemento <a>
@@ -33,24 +32,52 @@ const criarItem = item =>{
     //neste caso innerHtml esta funcionando na memoria então não atualiza a patgina
     novoLink.innerHTML = `<img src="${item}">`;
     //adicionando novoLink no Container 
-    container.append(novoLink);
+    container.appendChild(novoLink);
 
 
-    //inserindo codigo html no container
-    /*
-        *entretando innerHTML trabalha direto no codigo fonte, apagando e 
-        *recarregando tudo a cada innerHTML.
-    */
-    //container.innerHTML += `
-    //    <a href="#imagem4" class="itens-galeria">
-    //        <img src="${item}">
-    //    </a>
-    //`;
 }
+//Função para criar Slide dentro do .container-slide
+const criarSlide = (item,indice,array) =>{
+    const container =  document.querySelector(".container-slide");
+    //criando um novo elemento<div> na memória
+    const novaDiv = document.createElement("div");
+    //adicionando uma classe a nova div
+    novaDiv.classList.add("slide");
+    //recebendo id com a função filtrar id
+    novaDiv.id = filtrarId(item);
+
+    //verificando se o indice é igual a 0 e retornando posição final do array ou indice anterior
+    const indiceAnterior = indice == 0 ? array.length - 1 : indice -1;
+    //recebendo id da imagem na posição retornada pelo indiceAnterior
+    const slideAnterior = filtrarId(array[indiceAnterior]);
+
+    //verificando se o indice é igual a ultima posição do array e retornando 0 se não + 1
+    const indiceSeguinte = indice == array.length -1 ?  0 : indice + 1;
+    //recebendo id da imagem na posição retornada pelo indiceSeguinte
+    const slideSeguinte = filtrarId(array[indiceSeguinte]);
+
+    //inserindo bloco html  na novaDiv
+    novaDiv.innerHTML = `
+        <div class="container-imagem">
+            <a href="#"class="fechar">&#10006;</a>
+            <a href="#${slideAnterior}"class="navegacao anterior">&#171;</a>
+            <img src="${item}">
+            <a href="#${slideSeguinte}"class="navegacao proximo">&#187;</a>
+        </div>
+    `
+    //criando nova div no container
+    container.appendChild(novaDiv);
+}
+
+
 
 //Função responsavel por apresentar imagens da galeria
 //percorrendo com .foreach, percorre array e manda pro callback
 const carregarGaleria = imagens => imagens.forEach(criarItem);
 
+const carregarSlide = imagens => imagens.forEach(criarSlide);
+
 carregarGaleria(imagens);
+carregarSlide(imagens);
+
 
